90f4022902aedd6394aff3bc54b291f45fd9c3b8
Fabio Tiriticco <fabio.europe@gmail.com>
2014-05-05 17:17:39 +0200

    about pagina + logo

---
+++ cloudmatch-demo-ios/About/CMDAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMDAboutViewController.h
+//  CloudMatch Demo
+//
+//  Created by Fabio Tiriticco on 5/05/2014.
+//  Copyright (c) 2014 CloudMatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMDAboutViewController : UIViewController
+
+@end
---
+++ cloudmatch-demo-ios/About/CMDAboutViewController.m
@@ -0,0 +1,49 @@
+//
+//  CMDAboutViewController.m
+//  CloudMatch Demo
+//
+//  Created by Fabio Tiriticco on 5/05/2014.
+//  Copyright (c) 2014 CloudMatch. All rights reserved.
+//
+
+#import "CMDAboutViewController.h"
+
+@interface CMDAboutViewController ()
+
+@end
+
+@implementation CMDAboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
